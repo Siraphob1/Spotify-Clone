2fe633fdcf62921d984cbae698017361f91b3b54
Siraphob <sirapop_t@hotmail.com>
2024-02-01 23:49:55 +0700

    Login create login component

---
+++ src/component/Login.tsx
@@ -0,0 +1,28 @@
+import queryString from "query-string";
+
+const Login = () => {
+  const query = queryString.stringify({
+    client_id: import.meta.env.VITE_CLIENT_ID,
+    response_type: "code",
+    redirect_uri: import.meta.env.VITE_HOST,
+    scope: `streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public`,
+  });
+
+  const urlAuth = `https://accounts.spotify.com/authorize?${query}`;
+
+  return (
+    <main className="bg-[#121313] text-white w-[700px]  h-[700px] rounded-xl flex flex-col items-center justify-center gap-y-[1rem]">
+      <h1 className="text-[3rem]">Clone Spotify</h1>
+      <p>Please click to login</p>
+
+      <a
+        href={urlAuth}
+        className="bg-[#1ED661] text-black font-bold px-[2rem] py-[0.5rem] rounded-2xl hover:bg-opacity-70"
+      >
+        Login
+      </a>
+    </main>
+  );
+};
+
+export default Login;
