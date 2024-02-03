196a0d5c30cc19c8543de19756faebfe0cd26870
Siraphob <sirapop_t@hotmail.com>
2024-02-03 17:45:36 +0700

    SideBar apply Bottom component

--- src/component/sidebar/SideBar.tsx
+++ src/component/sidebar/SideBar.tsx
@@ -1,13 +1,11 @@
-// type Props = {
-//   className: string;
-
+import Bottom from './Bottom';
 import Top from './Top';
 
-// };
 const SideBar = () => {
   return (
-    <div className=" h-[calc(100vh-1rem)] w-[300px] ">
+    <div className=" h-[calc(100vh-1rem)] w-[300px] flex flex-col gap-y-[0.5rem] ">
       <Top className="bg-BgFirstPage rounded-xl" />
+      <Bottom className="bg-BgFirstPage w-full h-full rounded-xl p-[1rem] " />
     </div>
   );
 };
