577f7255e2e939814803d4b8e28e5657e1bcf0fc
Siraphob <sirapop_t@hotmail.com>
2024-02-03 15:53:50 +0700

    Login & Refresh update url

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { LoginResponse } from '../type/login';
+import { env } from '../environment/enviroment';
 
 export const loginAPI = async (code: string): Promise<LoginResponse> => {
-  const url = 'http://localhost:3500/login';
+  const url = `${env.BackendURL}/login`;
   const resp = await axios.post(url, { code });
   return resp.data;
 };
--- src/api/refresh.ts
+++ src/api/refresh.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { RefreshResponse } from '../type/login';
+import { env } from '../environment/enviroment';
 
 export const refreshTokenAPI = async (
   refreshToken: string
 ): Promise<RefreshResponse> => {
-  const url = 'http://localhost:3500/refresh';
+  const url = `${env.BackendURL}/refresh`;
   const resp = await axios.post(url, { refreshToken });
   return resp.data;
 };
