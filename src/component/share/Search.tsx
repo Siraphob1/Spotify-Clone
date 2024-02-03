df64cd414187bc049fba2bbbfbdcbf3b39bf9645
Siraphob <sirapop_t@hotmail.com>
2024-02-03 16:42:43 +0700

    Search create component

---
+++ src/component/share/Search.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent } from 'react';
+import { IoMdClose } from 'react-icons/io';
+import { IoSearchOutline } from 'react-icons/io5';
+
+type Props = {
+  placeholder: string;
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+};
+const Search = ({ placeholder, search, setSearch }: Props) => {
+  const handleClear = () => {
+    setSearch('');
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className="relative  max-w-xs">
+      <IoSearchOutline className="absolute top-[50%] translate-y-[-50%] left-[1rem] text-[1.4rem]" />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="input input-bordered input-accent text-[0.9rem] w-full  rounded-full pl-[3rem] pr-[2rem]"
+        value={search}
+        onChange={handleChange}
+      />
+      <IoMdClose
+        className="absolute top-[50%] translate-y-[-50%] right-[1rem] text-[1.4rem] cursor-pointer"
+        onClick={handleClear}
+      />
+    </div>
+  );
+};
+
+export default Search;
