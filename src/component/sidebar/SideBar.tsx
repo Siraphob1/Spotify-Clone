7c21329365d797ab8bd280b2b342bd0889f773e3
Siraphob <sirapop_t@hotmail.com>
2024-02-03 17:11:43 +0700

    SideBar mock UI Top section

---
+++ src/component/sidebar/SideBar.tsx
@@ -0,0 +1,15 @@
+// type Props = {
+//   className: string;
+
+import Top from './Top';
+
+// };
+const SideBar = () => {
+  return (
+    <div className=" h-[calc(100vh-1rem)] w-[300px] ">
+      <Top className="bg-BgFirstPage rounded-xl" />
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ src/component/sidebar/Top.tsx
@@ -0,0 +1,48 @@
+import { RiSearchEyeLine, RiSearchLine } from 'react-icons/ri';
+import { IoHome, IoHomeOutline } from 'react-icons/io5';
+import { useState } from 'react';
+
+type Props = {
+  className: string;
+};
+
+const Top = ({ className }: Props) => {
+  const listMenu = [
+    {
+      label: 'Home',
+      iconClicked: <IoHome />,
+      iconNormal: <IoHomeOutline />,
+    },
+    {
+      label: 'Search',
+      iconClicked: <RiSearchEyeLine />,
+      iconNormal: <RiSearchLine />,
+    },
+  ];
+
+  const [selectMenu, setSelectMenu] = useState<string>('');
+  return (
+    <div className={className}>
+      {listMenu.map((element) => {
+        return (
+          <div
+            className="flex items-center gap-x-[1rem] p-[1rem] cursor-pointer "
+            onClick={() => {
+              setSelectMenu(element.label);
+              console.log(element.label);
+            }}
+          >
+            <span className="text-[1.4rem]">
+              {selectMenu === element.label
+                ? element.iconClicked
+                : element.iconNormal}
+            </span>
+            <span>{element.label}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Top;
